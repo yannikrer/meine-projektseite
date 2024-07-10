bceecb480f4733ebbedef8931606eed1b6d10e69
yannik.scherer <yannik.scherer@edu.tbz.ch>
2024-07-10 20:04:30 +0200

    Initial commit

---
+++ KitTicker/html/js/shirts.js
@@ -0,0 +1,46 @@
+const shirts = [{
+        name: "Liverpool Trainingswear",
+        price: "50",
+        description: "Beschreibung von Shirt 1",
+        image: "images/liv.png",
+        imageBack: "images/liv2.png",
+        brand: "Marke 1",
+        league: "Liga 1",
+        season: "2024",
+        player: "Spieler 1"
+    },
+    {
+        name: "Liverpool Trainingswear",
+        price: "50",
+        description: "Beschreibung von Shirt 1",
+        image: "images/liv.png",
+        imageBack: "images/liv2.png",
+        brand: "Marke 1",
+        league: "Liga 1",
+        season: "2024",
+        player: "Spieler 1"
+    },
+    {
+        name: "Liverpool Trainingswear",
+        price: "50",
+        description: "Beschreibung von Shirt 1",
+        image: "images/liv.png",
+        imageBack: "images/liv2.png",
+        brand: "Marke 1",
+        league: "Liga 1",
+        season: "2024",
+        player: "Spieler 1"
+    },
+    {
+        name: "Liverpool Trainingswear",
+        price: "50",
+        description: "Beschreibung von Shirt 1",
+        image: "images/liv.png",
+        imageBack: "images/liv2.png",
+        brand: "Marke 1",
+        league: "Liga 1",
+        season: "2024",
+        player: "Spieler 1"
+    }
+
+];
