ad8dc987d072d2d7116e130de0ec15864c4282cb
yannik.scherer <yannik.scherer@edu.tbz.ch>
2024-07-11 20:20:36 +0200

    cart

--- js/shirts.js
+++ js/shirts.js
@@ -1,46 +1,116 @@
 const shirts = [{
-        name: "Liverpool Trainingswear",
-        price: "50",
-        description: "Beschreibung von Shirt 1",
+        name: "2017-18 Liverpool 125 Years Home Shirt",
+        price: "79.50",
         image: "images/liv.png",
         imageBack: "images/liv2.png",
-        brand: "Marke 1",
-        league: "Liga 1",
-        season: "2024",
-        player: "Spieler 1"
+        brand: "New Balance",
+        league: "Premier League",
+        season: "2017/18",
+        player: "M.Salah"
     },
     {
-        name: "Liverpool Trainingswear",
-        price: "50",
-        description: "Beschreibung von Shirt 1",
-        image: "images/liv.png",
-        imageBack: "images/liv2.png",
-        brand: "Marke 1",
-        league: "Liga 1",
-        season: "2024",
-        player: "Spieler 1"
+        name: "2008-09 Barcelona Home Shirt Messi",
+        price: "149.50",
+        image: "images/messi.png",
+        imageBack: "images/messi2.png",
+        brand: "Nike",
+        league: "National",
+        season: "2008/09",
+        player: "L.Messi"
     },
     {
-        name: "Liverpool Trainingswear",
-        price: "50",
-        description: "Beschreibung von Shirt 1",
-        image: "images/liv.png",
-        imageBack: "images/liv2.png",
-        brand: "Marke 1",
-        league: "Liga 1",
-        season: "2024",
-        player: "Spieler 1"
+        name: "2004-06 Portugal Home Shirt",
+        price: "150",
+        image: "images/ronny.png",
+        imageBack: "images/ronny2.png",
+        brand: "Nike",
+        league: "National",
+        season: "2004/06",
+        player: "C.Ronaldo"
     },
     {
-        name: "Liverpool Trainingswear",
-        price: "50",
-        description: "Beschreibung von Shirt 1",
-        image: "images/liv.png",
-        imageBack: "images/liv2.png",
-        brand: "Marke 1",
+        name: "2014-15 Olimpo Home Shirt",
+        price: "56.99",
+        image: "images/olim.png",
+        imageBack: "images/olim2.png",
+        brand: "Kappa",
         league: "Liga 1",
-        season: "2024",
-        player: "Spieler 1"
+        season: "2014/15",
+        player: "-"
+    },
+    {
+        name: "1998-00 Chelsea Third Shirt",
+        price: "89.75",
+        image: "images/chel.png",
+        imageBack: "images/chel.png",
+        brand: "Umbro",
+        league: "Premier League",
+        season: "1998/00",
+        player: "-"
+    },
+    {
+        name: "2006-08 Liverpool Home Shirt",
+        price: "67.35",
+        image: "images/liv08.png",
+        imageBack: "images/liv082.png",
+        brand: "Kappa",
+        league: "Premier League",
+        season: "2006/08",
+        player: "-"
+    },
+    {
+        name: "2008-10 Switzerland Home Shirt",
+        price: "89.00",
+        image: "images/sui.png",
+        brand: "Puma",
+        league: "National",
+        season: "2008/10",
+        player: "-"
+    },
+    {
+        name: "1990-92 Switzerland Home Shirt",
+        price: "290.99",
+        image: "images/sui2.png",
+        brand: "-",
+        league: "National",
+        season: "1990/92",
+        player: "-"
+    },
+    {
+        name: "2005-07 Germany Away Shirt",
+        price: "156.75",
+        image: "images/ger.png",
+        brand: "Adidas",
+        league: "National",
+        season: "2005/07",
+        player: "P.Lahm"
+    },
+    {
+        name: "1996-98 Germany WM2006 Away Shirt",
+        price: "350.25",
+        image: "images/ger2.png",
+        brand: "Adidas",
+        league: "National",
+        season: "1996/98",
+        player: "-"
+    },
+    {
+        name: "2024-25 Switzerland Away Shirt",
+        price: "104.99",
+        image: "images/gar.png",
+        brand: "Puma",
+        league: "National",
+        season: "2024/25",
+        player: "G.Xhaka"
+    },
+    {
+        name: "1995-96 Ajax Umbro Training",
+        price: "156.45",
+        image: "images/aj.png",
+        brand: "Umbro",
+        league: "Eredevise",
+        season: "1995/96",
+        player: "-"
     }
 
 ];
